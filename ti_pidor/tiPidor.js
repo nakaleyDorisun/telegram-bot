1b7f0ee543fc3e4884951715d4f61a6fd28d7564
nakaleyDorisun <audioreklama39@gmail.com>
2024-11-22 01:14:09 +0400

    first

---
+++ ti_pidor/tiPidor.js
@@ -0,0 +1,17 @@
+export const tiPidor = () => {
+  const members = [
+    "nakaleydorisun",
+    "nastyatachka",
+    "Space_191",
+    "vaalltteer",
+    "vitaaredz",
+    "volosheen",
+    "KobernikDmitriy",
+    "underscorehyypia",
+    "artfeefteen",
+    "max_trj",
+  ];
+  let random = Math.floor(Math.random() * 10);
+  console.log(random);
+  return members[random];
+};
